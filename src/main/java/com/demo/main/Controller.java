167afcc555889f46850db8d74140b56ab81afd79
guptasourya <121170479+guptasourya@users.noreply.github.com>
2023-03-22 16:03:03 +0530

    added my project

---
+++ src/main/java/com/demo/main/Controller.java
@@ -0,0 +1,17 @@
+package com.demo.main;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+public class Controller {
+	
+	@GetMapping("/")
+	public String hello() {
+		
+	return "welcome to my project";
+	
+		}
+}
